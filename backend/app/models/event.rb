bc5711f0dd74e98636b320c0ce701a8a5f8d1022
Freddy <febacigalupo1@miuandes.cl>
2024-08-21 23:17:48 -0400

    event destroy fix - dependencies

--- backend/app/models/event.rb
+++ backend/app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :bar
   has_many :attendances
-  has_many :users, through: :attendances
+  has_many :users, through: :attendances, dependent: :destroy
 
   has_one_attached :flyer
 
