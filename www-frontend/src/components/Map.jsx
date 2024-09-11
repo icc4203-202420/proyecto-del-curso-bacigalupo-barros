1df076cfab11729f535473fc22a9be0707370c51
Freddy <febacigalupo1@miuandes.cl>
2024-09-11 11:17:54 -0300

    first map implementations for 1.4

--- www-frontend/src/components/Map.jsx
+++ www-frontend/src/components/Map.jsx
@@ -1,12 +1,40 @@
-import { useEffect, useRef, useState } from 'react';
-import { useLoadGMapsLibraries } from './hooks/useLoadGMapsLibraries';
-import { MarkerClusterer } from '@googlemaps/markerclusterer';
-import { MAPS_LIBRARY, MARKER_LIBRARY } from './constants';
+import { useEffect, useRef } from 'react';
+import { useLoadGMapsLibraries } from './useLoadGMapsLibraries';
+import { MAPS_LIBRARY } from './constants';
+import { randomCoordinates } from './utils';
 
-const MAP_CENTER = { lat: -33.45694, lng: -70.64827 }; 
+const MAP_CENTER = { lat: -31.56391, lng: -70.64827 };
 
-const Map = ({ bars }) => {
+const Map = () => {
+  const libraries = useLoadGMapsLibraries();
+  const mapNodeRef = useRef();
+  const mapRef = useRef();
 
-}
+  useEffect(() => {
+    if (!libraries) {
+      return;
+    }
+
+    const { Map } = libraries[MAPS_LIBRARY];
+    mapRef.current = new Map(mapNodeRef.current, {
+      mapId: 'DEMO_MAP_ID',
+      center: MAP_CENTER,
+      zoom: 7,
+    });
+
+    const { Marker } = libraries['marker']; // Asegúrate de que la biblioteca de marcadores esté cargada
+    const randomPosition = randomCoordinates(MAP_CENTER, 0.1); // Rango de 0.1 grados
+    new Marker({
+      position: randomPosition,
+      map: mapRef.current,
+    });
+  }, [libraries]);
+
+  if (!libraries) {
+    return <h1>Cargando. . .</h1>;
+  }
+
+  return <div ref={mapNodeRef} style={{ width: '100vw', height: '100vh' }} />;
+};
 
 export default Map;
